77591649c2d36e28143b2878c966bbd384dc0a09
Daniel Popovici <dpopovici601@gmail.com>
2022-08-02 22:21:08 -0400

    carousel somewhat works now, if you resize the image you were on is no longer in the center

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 let images = [...document.querySelectorAll('.imgDiv')];
 let slider = document.querySelector('#slider');
-
+let sliderItem = document.querySelector('.sliderItem');
+let previousButton = document.querySelector('#previousButton');
+let nextButton = document.querySelector('#nextButton');
+let translateX = 0;
+let imageIndex = 1;
 
 images.forEach((img,index) =>{
     img.style.backgroundImage = `url(./images/img${index+1}.jpeg)`;
 });
+
+previousButton.addEventListener('click',() => {
+    imageIndex -= 1;
+    translateX += sliderItem.offsetWidth;
+    console.log(translateX);
+    slider.style.transform = `translateX(${translateX}px)`;
+});
+
+nextButton.addEventListener('click',() => {
+    imageIndex -= 1;
+    translateX -= sliderItem.offsetWidth;
+    console.log(translateX);
+    slider.style.transform = `translateX(${translateX}px)`;
+});
+
+
+
+
 
 
 
